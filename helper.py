d4a46b1f6aa7abe3669fc8f0b25d4a60c012267c
Matthew Fante <matthew.fante@gmail.com>
2020-04-10 01:53:41 -0400

    Cleanup and efficiency improvements, added extra helper functions for later

--- helper.py
+++ helper.py
@@ -5,54 +5,35 @@ import sys
 def five_dots(sec):
     elipsis = '.....'
     for dot in elipsis:
-        sleep(.2)
+        sleep(sec/len(elipsis))
         print(dot, end = '')
-
-def check_dependency_prompt(dependency_list):
-    yn = ''
-    
-#     print("Greetings! I can help you install or update the dependencies for this notebook!")
-    
-    while yn.lower() not in ['yes', 'no']:
-        clear_output()
-        yn = input("Do you want to check dependencies for updates? ")
-    
-    if yn.lower() == 'yes':
-        clear_output()
-        print("Great! Let's get started", end='')
-        five_dots(1)
-        check_dependencies(dependency_list)
         
-    elif yn.lower() == 'no':
-        clear_output()
-        print("Okay! I won't check dependencies right now.")
-        
-    yn = ''
-        
-        
-def check_dependencies(list_of_dependencies):
-    dependency_summary = []
+def check_dependencies(dependency_list):
     ready = 0
-    for library in list_of_dependencies:
+    for library in dependency_list:
         clear_output()
         print('Checking ' + library, end = '')
-        five_dots(1)
+        five_dots(.5)
         clear_output()
         
         if library in sys.modules:
-            dependency_summary.append(library + " is ready to use")
             ready += 1
         elif library not in sys.modules:
-            dependency_summary.append(library + " is NOT ready to use")
-    for item in dependency_summary:
-        print(item)
-    if ready == len(list_of_dependencies):
-        print("\nAll dependencies are ready... Enjoy!")
+            pass
+    if ready == len(dependency_list):
+        print("All dependencies are ready... Enjoy!")
     else: 
-        print("\nSome dependencies are missing... please run the INSTALL/UPDATE DEPENDENCIES cell at the bottom of this notebook before proceeding!")
+        print("Some dependencies are missing... Please run the INSTALL/UPDATE DEPENDENCIES cell at the bottom of this notebook before proceeding!")
+
+        
+def csv_export(df, file_name):
+    df.to_csv(file_name, index=True)
     
     
+# def xls_export(df, file_name, sheet_name):
+#     df.to_excel(file_name, sheet_name=sheet_name)
     
+        
 # This could come in handy for a nice clean excel export once everything is said and done.
 # df2 = df1.copy()
 # with pd.ExcelWriter('output.xlsx') as writer:  
@@ -61,12 +42,14 @@ def check_dependencies(list_of_dependencies):
 
 # df.to_excel("output.xlsx", sheet_name='Sheet_name_1')  
 
+ 
 
+# def xls_export(list_of_dfs, file_name):
+#     with pd.ExcelWriter(file_name) as writer:  
+#         i = 0
+#         for dataframe in list_of_dfs:
+#             dataframe.to_excel(writer, sheet_name="sheet")
+#             i +=1
+# list_of_dfs = [main_seasons_df, all_stars_df]
 
-        
-def csv_export(df, file_name):
-    df.to_csv(file_name, index=True)
-    
-    
-def xls_export(df, file_name, sheet_name):
-    df.to_excel(file_name, sheet_name=sheet_name)
+# xls_export(list_of_dfs, 'output.xlsx')
